d41d9f3e92c1c2f1aab5706ef7b95f93462e0107
Josue Retamozo Vargas <josretamozovar@gmail.com>
2022-02-24 23:08:18 -0500

    trabajando con useState

--- src/Pages/Calculator/Calculator.jsx
+++ src/Pages/Calculator/Calculator.jsx
@@ -1,91 +1,92 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Boton from '../../Components/Boton/Boton';
 import "./styles.scss"
 
 const Calculator = () => {
 
+  // const handleExecuteOperation = (value) => {
+  //   console.log("ENTRAR AL EJECUTAR OPERACION")
+  //   console.log(value);
+  //   handleAsignValue(value);
+  //   if (!isFirstValue) {
 
-  const [resultado, setResultado] = useState(0);
+  //     console.log("EJECUTAR OPERACION")
+
+  //     handleAsignValue(value);
+  //     switch (operation) {
+  //       case '+':
+  //         handleSum(value1, value2);
+
+  //         break;
+  //       default:
+  //         break;
+  //     }
+  //   } else if (isFirstValue) {
+  //     handleAsignValue(value);
+  //   }
+
+  // }
+
+  const [resultado, setResultado] = useState(null);
   const [value1, setValue1] = useState(0);
   const [value2, setValue2] = useState(0);
-  const [operation, setOperation] = useState('');
-
   const [isFirstValue, setIsFirstValue] = useState(true);
+ 
 
-  const handleSum = (value1, value2) => {
-    console.log(value1, value2);
-    console.log("SUMA")
-    setResultado(value1 + value2);
-  }
+  const handleAssignValue = (value) => {
 
-  const handleAsignValue = (value) => {
-    console.log("ASIGNAR VALOR")
-    // isFirstValue ? setValue1(value) : setValue2(value);
-    // console.log(isFirstValue ? value1 + "PrimerValor" : value2 + "SegundoValor");
-    // setIsFirstValue(!isFirstValue);
+    console.log("ENTRAR AL ASIGNAR VALOR")
 
-    if (isFirstValue === true) {
+    if (value1 === 0 && isFirstValue) {
+      console.log("ASIGNAR VALOR 1", value)
       setValue1(value);
-      console.log("Primer Valor", value1);
       setIsFirstValue(false);
-    } else if (isFirstValue === false) {
-      console.log("Segundo Valor", value2);
-      setValue2(value)
-      setIsFirstValue(true);
+    } else {
+      console.log("ASIGNAR VALOR 2", value)
+      setValue2(value);
+      setIsFirstValue(prevState => !prevState);
     }
   }
 
-  const handleExecuteOperation = (value) => {
-    console.log("ENTRAR AL EJECUTAR OPERACION")
-    console.log(value);
-    //e.preventDefault();
-    handleAsignValue(value);
-    if (!isFirstValue) {
-      console.log("EJECUTAR OPERACION")
-      handleAsignValue(value);
-      // lanzo la operacion
-
-      switch (operation) {
-        case '+':
-          handleSum(value1, value2);
-        default:
-          break;
-      }
+  const handleExecuteOperation = (operation) => {
+    switch (operation) {
+      case '+':
+        setResultado(value1 + value2);
+        break;
+      default:
+        break;
     }
-
   }
 
   const reset = () => {
-    //e.preventDefault();
     setValue1(0);
     setValue2(0);
     setResultado(0);
-    setIsFirstValue(true);
   }
 
   return (
     <main>
       <div className='contenedor__resultado'>
-        <div>{resultado}</div>
+        <div>{resultado || 0}</div>
       </div>
       <div className="contenedor">
         <Boton className="item" titulo="AC" evClick={reset} />
         <Boton className="item" titulo="+/-" />
         <Boton className="item" titulo="%" />
         <Boton className="item" titulo="/" />
-        <Boton className="item" titulo="7" evClick={() => handleExecuteOperation(7)} />
-        <Boton className="item" titulo="8" evClick={() => handleExecuteOperation(8)} />
-        <Boton className="item" titulo="9" />
+        <Boton className="item" titulo="7" evClick={() => handleAssignValue(7)} />
+        <Boton className="item" titulo="8" evClick={() => handleAssignValue(8)} />
+        <Boton className="item" titulo="9" evClick={() => handleAssignValue(9)} />
         <Boton className="item" titulo="-" />
-        <Boton className="item" titulo="4" />
-        <Boton className="item" titulo="5" />
-        <Boton className="item" titulo="6" />
+        <Boton className="item" titulo="4" evClick={() => handleAssignValue(4)} />
+        <Boton className="item" titulo="5" evClick={() => handleAssignValue(5)} />
+        <Boton className="item" titulo="6" evClick={() => handleAssignValue(6)} />
         <Boton className="item" titulo="x" />
-        <Boton className="item" titulo="1" />
-        <Boton className="item" titulo="2" />
-        <Boton className="item" titulo="3" />
-        <div className="boton" >0</div>
-        <Boton className="item" titulo="+" evClick={() => setOperation("+")} />
+        <Boton className="item" titulo="1" evClick={() => handleAssignValue(1)} />
+        <Boton className="item" titulo="2" evClick={() => handleAssignValue(2)} />
+        <Boton className="item" titulo="3" evClick={() => handleAssignValue(3)} />
+        <div className="boton" onClick={() => handleAssignValue(0)}>0</div>
+        <Boton className="item" titulo="+" evClick={() => handleExecuteOperation("+")} />
         <Boton className="item" titulo="." />
         <Boton className="item" titulo="=" />
       </div>
